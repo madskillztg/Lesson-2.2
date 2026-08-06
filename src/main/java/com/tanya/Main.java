adc69ced4d77cd92170e84cd72ee5a3e73771e93
Tanya Gainutdinova <tanyagainutdinova@Tanya.local>
2026-08-06 23:39:39 +0100

    Add printColor

--- src/main/java/com/tanya/Main.java
+++ src/main/java/com/tanya/Main.java
@@ -4,11 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
+        printColor();
     }
-
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -5;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 50;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
     }
 }
