b540f94c877a50d2e73f082bc4be6724515956b8
Tanya Gainutdinova <tanyagainutdinova@Tanya.local>
2026-08-07 16:50:28 +0100

    Add diagonal matrix initialization

--- src/main/java/com/tanya/Main.java
+++ src/main/java/com/tanya/Main.java
@@ -15,6 +15,7 @@ public class Main {
         invertArray();
         fillArray();
         changeArray();
+        fillDiagonal();
     }
     public static void printThreeWords() {
         System.out.println("Orange");
@@ -137,6 +138,20 @@ public static void changeArray() {
     }
 
     System.out.println();
+}
+public static void fillDiagonal() {
+    int[][] array = new int[5][5];
+
+    for (int i = 0; i < array.length; i++) {
+        array[i][i] = 1;
+    }
+
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+            System.out.print(array[i][j] + " ");
+        }
+        System.out.println();
+    }
 }
 }
 
