c7f1b84d4be4289d2b8efccca2dbe2fe92f6c7ed
Tanya Gainutdinova <tanyagainutdinova@Tanya.local>
2026-08-14 01:52:38 +0100

    Complete lesson 2.3

---
+++ src/main/java/com/tanya/Product.java
@@ -0,0 +1,31 @@
+package com.tanya;
+
+import java.time.LocalDate;
+
+public class Product {
+    private String name;
+    private LocalDate productionDate;
+    private String manufacturer;
+    private String country;
+    private double price;
+    private boolean reserved;
+
+    public Product(String name, LocalDate productionDate, String manufacturer, String country, double price, boolean reserved) {
+        this.name = name;
+        this.productionDate = productionDate;
+        this.manufacturer = manufacturer;
+        this.country = country;
+        this.price = price;
+        this.reserved = reserved;
+    }
+
+    public void printInfo() {
+        System.out.println("Название: " + name);
+        System.out.println("Дата производства: " + productionDate);
+        System.out.println("Производитель: " + manufacturer);
+        System.out.println("Страна происхождения: " + country);
+        System.out.println("Цена: " + price);
+        System.out.println("Забронирован: " + reserved);
+    }
+}
+
