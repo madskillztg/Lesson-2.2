cf42dc42418aad9f771b768a205e346bfc9e22f4
tanyagainutdinova <madskillztg@gmail.com>
2026-08-30 17:39:59 +0100

    Add Lesson 2.7 programs

---
+++ src/test/java/com/tanya/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.tanya;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        assertEquals(8, Calculator.add(5, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, Calculator.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, Calculator.multiply(5, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, Calculator.divide(6, 3));
+    }
+}
+
+
